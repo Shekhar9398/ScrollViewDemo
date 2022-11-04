809bb70615efe29fbdd3da99ec4099636a839dd4
Shekhar9398 <117426760+Shekhar9398@users.noreply.github.com>
2022-11-04 11:05:12 +0530

    Add files via upload

---
+++ ScrollView/ScrollView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ScrollView
+//
+//  Created by Shraddha Manwar on 14/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+//    @IBOutlet weak var myScrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
